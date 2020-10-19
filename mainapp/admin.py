302c2ef1d7b320ef67d06f96916809d9055ec39d
Алексей <aiex.0115970@gmail.com>
2020-10-19 12:44:13 +0500

    Add other

--- mainapp/admin.py
+++ mainapp/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
-from mainapp.models import Category, Web, Dedic
+from mainapp.models import Category, Web, Dedic, Vds
 
 admin.site.register(Category)
 admin.site.register(Web)
 admin.site.register(Dedic)
+admin.site.register(Vds)
 
--- mainapp/models.py
+++ mainapp/models.py
@@ -46,3 +46,22 @@ class Dedic(models.Model):
 
     def __str__(self):
         return self.name
+
+class Vds(models.Model):
+    category = models.ForeignKey(Category,
+                                 on_delete=models.CASCADE)
+    name = models.CharField(max_length=128)
+    desc = models.TextField(blank=True)
+    prise = models.IntegerField(default=0)
+    cpu = models.CharField(max_length=128)
+    core = models.CharField(max_length=128)
+    ram = models.CharField(max_length=128)
+    disk = models.CharField(max_length=128)
+    ipv4 = models.CharField(max_length=128)
+    traffic = models.CharField(max_length=128)
+    location = models.CharField(max_length=128)
+    ddos = models.CharField(max_length=128)
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
