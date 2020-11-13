8daa3cad3e79ff39bd8b3b237d4ee3da85ae73a6
Алексей <aiex.0115970@gmail.com>
2020-11-13 23:33:34 +0500

    UPDATE

--- authapp/urls.py
+++ authapp/urls.py
@@ -1,11 +1,12 @@
 import authapp.views as authapp
 from django.urls import path
+from django.contrib.auth.models import User
 
 app_name = 'authapp'
 
 urlpatterns = [
-    path('login/', authapp.login, name='login'),
-    path('logout/', authapp.logout, name='logout'),
-    path('register/', authapp.register, name='register'),
-    path('profile/', authapp.profile, name='profile'),
+        path('login/', authapp.login, name='login'),
+        path('logout/', authapp.logout, name='logout'),
+        path('register/', authapp.register, name='register'),
+        path('profile/', authapp.profile, name='profile'),
 ]
